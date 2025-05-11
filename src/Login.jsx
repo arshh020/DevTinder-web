99e881bb5f0a2a6aac53c354b22559ef045417ae
Arsh Gogia <arshgogia20@gmail.com>
2025-05-12 00:03:40 +0400

    axios init: login form & API

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,63 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("gogs@gmail.com");
+  const [password, setPassword] = useState("Arsh@1234");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input text-gray-500"
+                placeholder="abc@gmail.com"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input text-gray-500"
+                placeholder="Abc@123"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
